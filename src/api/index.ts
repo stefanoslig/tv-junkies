4ca8885035dd4b754cf88a4db46118f97b5888d1
Stefanos Lignos <stefanoslignos@gmail.com>
2022-12-08 22:44:04 +0100

    feat: Migrate to TMDB API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,3 @@
 export * from "./fetch-wrapper";
+export * from "./types";
+export * from "./constants";
--- src/api/types.ts
+++ src/api/types.ts
@@ -1,101 +1,135 @@
-export const Genres = ["Drama", "Comedy", "Science-Fiction", "Sports"];
-
-export interface SearchShow {
-  score: number;
-  show: Show;
+export interface Query {
+  type: MediaType;
+  title: string;
+  query: string;
 }
 
-export type Shows = Array<SearchShow>;
+export type MediaType = "movie" | "tv";
 
-export interface Show {
-  id: number;
-  url: string;
-  name: string;
-  type: string;
-  language: string;
-  genres: Array<string>;
-  status: string;
-  runtime: number;
-  premiered: string;
-  officialSite: string;
-  schedule: Schedule;
-  rating: Rating;
-  weight: number;
-  network: Network;
-  webChannel: string | null;
-  externals: Externals;
-  image: Image;
-  summary: string;
-  updated: number;
-  _links: Links;
-  _embedded: Cast; // Cast is not the only data we can embed. The other types are not included in this app.
-}
-
-export interface Schedule {
-  time: string;
-  days: Array<string>;
-}
-
-export interface Rating {
-  average: number;
-}
-
-export interface Country {
-  name: string;
-  code: string;
-  timezone: string;
-}
-
-export interface Network {
-  id: number;
-  names: string;
-  country: Country;
-}
-
-export interface Externals {
-  tvrage: number;
-  thetvdb: number;
-  imdb: string;
-}
-
-export interface Image {
-  medium: string;
-  original: string;
-}
-
-export interface Links {
-  self?: Link;
-  previousepisode?: Link;
-  show?: Link;
-  character?: Link;
-}
-
-export interface Link {
-  href: string;
+export interface Media {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date?: string;
+  first_air_date?: string;
+  title: string;
+  name?: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  media_type?: MediaType;
+  // details
+  homepage?: string;
+  runtime?: number;
+  budget?: number;
+  revenue?: number;
+  status?: string;
+  genres?: Genre[];
+  production_companies?: any[];
+  videos?: {
+    results: Video[];
+  };
+  credits?: {
+    cast: Person[];
+    crew: Person[];
+  };
+  images?: {
+    backdrops: Image[];
+    posters: Image[];
+  };
+  external_ids?: ExternalIds;
+  // cast
+  character?: string;
 }
 
 export interface Person {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  profile_path: string;
+  popularity: number;
+  cast_id?: number;
+  job?: string;
+  character?: string;
+  credit_id: string;
+  order: number;
+  // details
+  also_known_as?: string[];
+  birthday?: string;
+  place_of_birth?: string;
+  homepage?: string;
+  biography?: string;
+  external_ids?: ExternalIds;
+  combined_credits?: {
+    cast?: Media[];
+    crew?: Media[];
+  };
+  images?: {
+    profiles: Image[];
+  };
+}
+
+export interface Video {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
+export interface Image {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: string;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+export interface ExternalIds {
+  imdb_id?: string;
+  facebook_id?: string;
+  instagram_id?: string;
+  twitter_id?: string;
+  linkedin_id?: string;
+  github_id?: string;
+  email?: string;
+  homepage?: string;
+}
+
+export interface PageResult<T> {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface Genre {
   id: number;
   name: string;
-  url: string;
-  country: Country;
-  birtday: string;
-  deathday: string | null;
-  image: Image;
-  _links: Links;
 }
-export interface Character {
-  id: number;
-  url: string;
-  name: string;
-  image: Image;
-  _links: Links;
+
+export interface QueryItem {
+  type: MediaType;
+  title: string;
+  query: string;
 }
-export interface Cast {
-  cast: Array<{
-    person: Person;
-    character: Character;
-    self: boolean;
-    voice: boolean;
-  }>;
+
+export interface Credits {
+  cast: Media[];
 }
